e088cc1d2db7334a2fcb3b80e1722ea6ceb5bf57
hello21 <hellono21@gmail.com>
2017-05-17 13:25:39 +0800

    fix missing validator;ipv6 ip fixed

--- app/api/ip.js
+++ app/api/ip.js
@@ -8,9 +8,13 @@ import db from '../db'
 const router = new Router()
 
 router.get('/ip', async (ctx) => {
+  let ip = ctx.ip
+  if (ip.substr(0, 7) == "::ffff:") {
+    ip = ip.substr(7)
+  }
   ctx.body = {
     request: ctx.request,
-    ip: ctx.ip,
+    ip,
   }
 })
 
