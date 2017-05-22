61f6eacc2311f65c184ceda914a13065fc35241b
hello21 <hellono21@gmail.com>
2017-05-22 10:54:58 +0800

    build docker

--- app/db/index.js
+++ app/db/index.js
@@ -4,6 +4,9 @@
 
 import Redis from 'ioredis'
 
-const redis = new Redis()
+const host = process.env.DBHOST || '127.0.0.1';
+const port = process.env.DBPORT || 6379;
+
+const redis = new Redis({port, host})
 
 export default redis
