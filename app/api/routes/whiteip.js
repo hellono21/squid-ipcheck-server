4177f469cc775deb1d0158c6dbdc19a167d30d7d
hello21 <hellono21@gmail.com>
2017-06-23 17:34:45 +0800

    old api supoort

--- app/api/routes/whiteip.js
+++ app/api/routes/whiteip.js
@@ -20,6 +20,21 @@ export default (router) => {
         ip,
       };
     })
+    .post('/ip', async ctx => {
+      const ip = ctx.request.body.ip;
+      if (!validator.isIP(ip)) {
+        ctx.throw(400, 'invalid ip address');
+      }
+
+      const prefix = configs.redis.prefix;
+      const key = `${prefix}:${ip}`;
+      //const ttl = 3600
+      await db.setex(key, ttl, 1)
+
+      ctx.body = {ip, ttl}
+
+      ctx.status = 201
+    })
     .get('/whiteips', async ctx => {
       ctx.body = await db.keys('squid*');
     })
